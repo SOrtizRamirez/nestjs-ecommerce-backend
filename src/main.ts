7ffc834f6f13f364642d4d3d768a7146c74bcd02
Sharon Ortiz <ortizramirezs@gmail.com>
2026-03-16 10:27:12 -0500

    Add Swagger support and update dependencies in package.json and package-lock.json

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
+
     const app = await NestFactory.create(AppModule);
+
     app.useGlobalFilters(new HttpExceptionFilter());
+
     app.useGlobalInterceptors(new TransformInterceptor());
+
+    // Swagger config
+    const config = new DocumentBuilder()
+        .setTitle('E-Commerce API')
+        .setDescription('NestJS E-Commerce Backend API Documentation')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(process.env.PORT ?? 3000);
+
     console.log('App running :)');
     console.log('Database connected successfully :D');
+    console.log('Swagger docs: http://localhost:3000/api');
+
 }
+
 bootstrap();
